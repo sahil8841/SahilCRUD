bf3ab803bbc5ba92ccd9cf1669f30a74486daec5
marsh <marsh@DESKTOP-FAQVT88>
2023-07-11 19:34:10 +0530

    Add project files.

---
+++ SahilCRUD/Home.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SahilCRUD.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>CRUD OPERATIONS</h1>
+        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Label ID="Label2" runat="server" Text="Mobile No."></asp:Label>
+        <asp:TextBox ID="txtmno" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Label ID="lblmsg" runat="server" Text=""></asp:Label>
+        <br /><br />
+        <asp:Button ID="btnadd" runat="server" Text="Add" OnClick="btnadd_Click" />
+        <asp:Button ID="btnupdate" runat="server" Text="Update" OnClick="btnupdate_Click" />
+        <asp:Button ID="btndelete" runat="server" Text="Delete" OnClick="btndelete_Click" />
+        <br /><br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="name" HeaderText="Name" />
+                <asp:BoundField DataField="mno" HeaderText="Mobile No." />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkselect" runat="server" CommandArgument='<%# Eval("empid") %>' OnClick="lnkselect_Click" >Select</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+            </Columns>
+
+        </asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ SahilCRUD/Home.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SahilCRUD
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FAQVT88\SQLEXPRESS;Initial Catalog=Testdb;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                display();
+            }
+        }
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string str = "insert into tblemp values('" + txtname.Text + "','" + txtmno.Text + "')";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.ExecuteNonQuery();
+            lblmsg.Text = "Record Inserted Successfully";
+            con.Close();
+            display();
+            cleartxt();
+        }
+
+        protected void lnkselect_Click(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            Session["id"] = btn.CommandArgument;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblemp", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+            if (dt.Rows.Count >= 0)
+            {
+                txtname.Text = dt.Rows[0]["name"].ToString();
+                txtmno.Text = dt.Rows[0]["mno"].ToString();
+
+            }
+            con.Close();
+        }
+
+        public void display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblemp", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            adp.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string str = "update tblemp set name='" + txtname.Text + "', mno='" + txtmno.Text + "'where empid='" + Session["id"] + "'";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.ExecuteNonQuery();
+            lblmsg.Text = "Record Updated Successfully";
+            con.Close();
+            display();
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from tblemp where empid='" + Session["id"] + "'", con);
+            cmd.ExecuteNonQuery();
+            lblmsg.Text = "Record Deleted";
+            con.Close();
+            display();
+        }
+
+        public void cleartxt()
+        {
+            txtmno.Text = "";
+            txtname.Text = "";
+        }
+    }
+}
